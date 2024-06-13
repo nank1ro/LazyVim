085badc9d786e5c26bbe2ac2da3393d5cca6d7f5
Alexandru Mariuti <alex@mariuti.com>
2024-06-13 17:35:35 +0200

    use catppuccin theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add catppuccin
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
