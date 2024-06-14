df84b99cd29a3950dc3ba575aca46a212b4b2206
Alexandru Mariuti <alex@mariuti.com>
2024-06-14 16:26:01 +0200

    add neotree mappings

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        mappings = {
+          ["J"] = function(state)
+            local tree = state.tree
+            local node = tree:get_node()
+            local siblings = tree:get_nodes(node:get_parent_id())
+            local renderer = require("neo-tree.ui.renderer")
+            renderer.focus_node(state, siblings[#siblings]:get_id())
+          end,
+          ["K"] = function(state)
+            local tree = state.tree
+            local node = tree:get_node()
+            local siblings = tree:get_nodes(node:get_parent_id())
+            local renderer = require("neo-tree.ui.renderer")
+            renderer.focus_node(state, siblings[1]:get_id())
+          end,
+          ["\\"] = "set_root",
+        },
+      },
+    },
+  },
+}
