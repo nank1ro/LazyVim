b8ecb6dad1e109d8a90e9dac4a0e617333ff2ebe
Alexandru Mariuti <alex@mariuti.com>
2024-10-21 17:23:14 +0100

    add telescope grep in folder and by extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,18 +10,32 @@ return {
       desc = "Workspace Diagnostics",
     },
   },
-  opts = {
-    defaults = {
-      mappings = {
-        i = {
-          ["<S-Down>"] = function(...)
-            require("telescope.actions").cycle_history_next(...)
-          end,
-          ["<S-Up>"] = function(...)
-            require("telescope.actions").cycle_history_prev(...)
-          end,
+  opts = function()
+    local custom_pickers = require("custom.telescope_custom_pickers")
+    return {
+      defaults = {
+        mappings = {
+          i = {
+            ["<S-Down>"] = function(...)
+              require("telescope.actions").cycle_history_next(...)
+            end,
+            ["<S-Up>"] = function(...)
+              require("telescope.actions").cycle_history_prev(...)
+            end,
+          },
         },
       },
-    },
-  },
+      pickers = {
+        live_grep = {
+          path_display = { "shorten" },
+          mappings = {
+            i = {
+              ["<c-f>"] = custom_pickers.actions.set_extension,
+              ["<c-l>"] = custom_pickers.actions.set_folders,
+            },
+          },
+        },
+      },
+    }
+  end,
 }
