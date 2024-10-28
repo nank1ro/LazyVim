1dd4ff9c1b369ec67bc562d5b2627f1dfe2b731f
Alexandru Mariuti <alex@mariuti.com>
2024-10-28 11:27:53 +0000

    disable supermaven and codeium

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,25 +1,26 @@
-return {
-  {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "Exafunction/codeium.nvim",
-        dependencies = {
-          "nvim-lua/plenary.nvim",
-          "hrsh7th/nvim-cmp",
-        },
-        config = function()
-          require("codeium").setup({})
-        end,
-      },
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, 2, {
-        name = "codeium",
-        group_index = 1,
-        priority = 50,
-      })
-    end,
-  },
-}
+return {}
+-- return {
+--   {
+--     "nvim-cmp",
+--     dependencies = {
+--       {
+--         "Exafunction/codeium.nvim",
+--         dependencies = {
+--           "nvim-lua/plenary.nvim",
+--           "hrsh7th/nvim-cmp",
+--         },
+--         config = function()
+--           require("codeium").setup({})
+--         end,
+--       },
+--     },
+--     ---@param opts cmp.ConfigSchema
+--     opts = function(_, opts)
+--       table.insert(opts.sources, 2, {
+--         name = "codeium",
+--         group_index = 1,
+--         priority = 50,
+--       })
+--     end,
+--   },
+-- }
--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,21 +1,22 @@
-return {
-  {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "supermaven-inc/supermaven-nvim",
-        config = function()
-          require("supermaven-nvim").setup({})
-        end,
-      },
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "supermaven",
-        group_index = 1,
-        priority = 100,
-      })
-    end,
-  },
-}
+return {}
+-- return {
+--   {
+--     "nvim-cmp",
+--     dependencies = {
+--       {
+--         "supermaven-inc/supermaven-nvim",
+--         config = function()
+--           require("supermaven-nvim").setup({})
+--         end,
+--       },
+--     },
+--     ---@param opts cmp.ConfigSchema
+--     opts = function(_, opts)
+--       table.insert(opts.sources, 1, {
+--         name = "supermaven",
+--         group_index = 1,
+--         priority = 100,
+--       })
+--     end,
+--   },
+-- }
