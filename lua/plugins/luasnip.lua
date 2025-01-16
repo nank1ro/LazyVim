442ea9acdfb854743d0f6b3af39d40117c13640a
Alexandru Mariuti <alex@mariuti.com>
2025-01-16 10:28:34 +0100

    update luasnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -3,6 +3,10 @@ return {
     "L3MON4D3/LuaSnip",
     config = function(_, _)
       require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/config/snippets" } }) -- load snippets paths
+      require("luasnip").config.setup({
+        update_events = "TextChanged,TextChangedI",
+        enable_autosnippets = true,
+      })
     end,
   },
 }
